c6d0b568e7a5595101c585487b23dfabeb7c6def
teamparmjeet <teamparmjeet@gmail.com>
2024-10-29 17:09:23 +0530

    new chnages

--- app/main/page/addquery/page.jsx
+++ app/main/page/addquery/page.jsx
@@ -6,6 +6,7 @@ import "react-phone-input-2/lib/style.css";
 import toast, { Toaster } from "react-hot-toast";
 import { useSession } from 'next-auth/react';
 import { Citylist } from "@/constants/City";
+import Address from "@/components/Address/Address"
 export default function Page() {
     const [branches, setBranches] = useState([]);
     const [allCourses, setAllCourses] = useState([]); // Store all courses
@@ -146,7 +147,6 @@ export default function Page() {
         const { name, value } = e.target;
 
         if (name === "deadline") {
-            // Format the date for display
             const formattedDate = new Date(value).toLocaleDateString("en-GB", {
                 day: "numeric",
                 month: "long",
@@ -280,7 +280,6 @@ export default function Page() {
                             />
                         </div>
 
-
                         <div className="sm:col-span-6 col-span-12">
                             <label className="block text-[15px] text-gray-700">Whatsapp Number</label>
                             <PhoneInput
@@ -393,19 +392,15 @@ export default function Page() {
 
                             </div>
                         )}
-
+                      
 
                         <div className="sm:col-span-6 col-span-12">
                             <label htmlFor="studentContact.address" className="block text-[15px] text-gray-700">
                                 Address
                             </label>
-                            <input
-                                type="text"
-                                name="studentContact.address"
-                                placeholder="Enter Address"
+                            <Address
                                 value={formData.studentContact.address}
                                 onChange={handleChange}
-                                className="block w-full px-2 py-2 text-gray-500 bg-white border border-gray-200  placeholder:text-gray-400 focus:border-[#6cb049] focus:outline-none focus:ring-[#6cb049] sm:text-sm"
                             />
                         </div>
 
@@ -466,7 +461,7 @@ export default function Page() {
                                 <select name="reference_name" value={formData.reference_name} id="" onChange={handleChange} className="block w-full px-2 py-2 text-gray-500 bg-white border border-gray-200  placeholder:text-gray-400 focus:border-[#6cb049] focus:outline-none focus:ring-[#6cb049] sm:text-sm">
                                     <option value="" disabled selected>Select Reference name</option>
                                     <option value={adminData.name}>Self</option>
-                                    {user.map((user,index) => (
+                                    {user.map((user, index) => (
                                         <option key={index} value={user.name}>{user.name}</option>
                                     ))}
                                 </select>
